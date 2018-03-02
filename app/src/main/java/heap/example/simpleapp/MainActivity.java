7e8bfa41bfa9b6e27e91bc5dd74909528151d1c5
Dan Li <dianwen@heapanalytics.com>
2018-03-02 13:22:46 -0800

    Instrumentation

--- app/src/main/java/heap/example/simpleapp/MainActivity.java
+++ app/src/main/java/heap/example/simpleapp/MainActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.heapanalytics.android.Heap;
+
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Heap.init(getApplicationContext(), "3320806168");
     setContentView(R.layout.activity_main);
   }
 

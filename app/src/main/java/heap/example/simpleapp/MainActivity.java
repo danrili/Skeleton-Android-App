6f7a14f008d3701ad105debfbae7c763616dcb8f
Dan Li <dianwen@heapanalytics.com>
2018-03-02 13:21:10 -0800

    Add onClickListener to button

--- app/src/main/java/heap/example/simpleapp/MainActivity.java
+++ app/src/main/java/heap/example/simpleapp/MainActivity.java
@@ -2,6 +2,8 @@ package heap.example.simpleapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void onButtonClick(View v) {
+    Toast toast = Toast.makeText(getApplicationContext(), "clicked!", Toast.LENGTH_SHORT);
+    toast.show();
   }
 }
